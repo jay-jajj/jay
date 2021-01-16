688894fc8a9f707dfdce2f6e1209e53aaf0aeb70
jay-jajj <asjh011969@gmail.com>
2021-01-16 16:46:39 +0900

    Please~

--- server/server.js
+++ server/server.js
@@ -30,18 +30,13 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === "production") {
   // Set static folder   
   // All the javascript and css files will be read and served from this folder
-  // React 로 build된 폴더를 연결한다.
-const root = require('path').join(__dirname, '..', 'client', 'build');
-
-// 정적 페이지를 보낸다.
-app.use(express.static(root));
+  app.use(express.static("client/build"));
   
   // index.html for all page routes    html or routing and naviagtion
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
-
 const connect = mongoose.connect(config.mongoURI,
     {
       useNewUrlParser: true, useUnifiedTopology: true,
