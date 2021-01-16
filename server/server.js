bd665b7107c8881f9279bfefe3bd92d707913e82
jay-jajj <asjh011969@gmail.com>
2021-01-16 15:38:19 +0900

    why

--- server/server.js
+++ server/server.js
@@ -24,11 +24,10 @@ const connect = mongoose.connect(config.mongoURI,
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-
   // Set static folder   
   // All the javascript and css files will be read and served from this folder
   app.use(express.static("client/build"));
-
+  
   // index.html for all page routes    html or routing and naviagtion
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
