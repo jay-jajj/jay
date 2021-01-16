acecc35294cb2193fb61c4ec65e70113a1b075b4
jay-jajj <asjh011969@gmail.com>
2021-01-16 18:57:28 +0900

    please

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const cors = require('cors');
 const port = process.env.PORT || 4000
 
 //body부분을 분석해주는 미들웨어
